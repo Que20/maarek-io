44d6ce37673468b94e07a3a702d660caa66c60be
Kevin Maarek <maarekkevin@gmail.com>
2022-11-08 10:50:00 +0100

    Added animations and cleaned warningson home page.

--- src/home/home.js
+++ src/home/home.js
@@ -3,7 +3,6 @@ import MainMenu from '../components/menu/menu';
 import Footer from '../components/footer/footer';
 import Parser from 'rss-parser';
 import './home.css';
-// import 'semantic-ui-css/semantic.min.css';
 import { Button } from 'semantic-ui-react';
 import { Link } from "react-router-dom";
 
@@ -41,22 +40,22 @@ class Home extends Component {
         return (
         <div>
             <MainMenu activeItem='home' />
-            <div class="container">
-            <div class="section">
-                <div class="presentation">
-                    <div class="ui two column stackable grid">
-                        <div class="center aligned column right-float">
-                            <img src="https://www.gravatar.com/avatar/c79d64c623fe657519e9ee790bfc7865?s=400" class="profile" alt="Gravatar profil" />
+            <div className="container">
+            <div className="section">
+                <div className="presentation">
+                    <div className="ui two column stackable grid">
+                        <div className="center aligned column right-float">
+                            <img src="https://www.gravatar.com/avatar/c79d64c623fe657519e9ee790bfc7865?s=400" className="profile" alt="Gravatar profil" />
                         </div>
-                        <div class="column">
+                        <div className="column">
                             <br />
-                            <span class="wave">👋</span>
-                            <h1 class="text-primary-color">
+                            <span className="wave hand-wave">👋</span>
+                            <h1 className="text-primary-color">
                                 Hi, my name is Kevin
                             </h1>
                             <p>
                                 I am a Software developer. 👨🏻‍💻<br />
-                                Currently working as a R&D Software Engineer at <a href="https://www.welcometothejungle.com/fr/companies/freebox"><span class="company">Freebox</span></a> in Paris 🇫🇷.
+                                Currently working as an iOS Engineer at <a href="https://www.welcometothejungle.com/fr/companies/ratp-smart-systems"><span className="company">RATP Smart Systems</span></a> in Paris 🇫🇷.
                                 <br /><br />
                                 Let me develop the app of your dreams 🧚🏻
                             </p>
@@ -88,37 +87,38 @@ class Home extends Component {
                         Looking for a dev for your next NFT project? You are in the right place.
                     </p>
                     <br/><br/>
-                    <div class="ui three column grid">
-                    <div class="column center aligned">
-                        <i class="react icon techLogo" style={{"color":"#61dafb"}}></i>
+                    <div className="ui three column grid">
+                    <div className="column center aligned tech">
+                        <i className="react icon techLogo" style={{"color":"#61dafb"}}></i>
                         <br/>
                         React
                     </div>
-                    <div class="column center aligned">
-                        <i class="node icon techLogo" style={{"color":"#43853d"}}></i>
+                    <div className="column center aligned tech">
+                        <i className="node icon techLogo" style={{"color":"#43853d"}}></i>
                         <br/>
                         NodeJS
                     </div>
-                    <div class="column center aligned">
-                        <i class="ethereum icon techLogo" style={{"color":"rgb(77, 28, 225)"}}></i>
+                    <div className="column center aligned tech">
+                        <i className="ethereum icon techLogo" style={{"color":"rgb(77, 28, 225)"}}></i>
                         <br/>
                         Solidity
                     </div>
                     </div>
                     <br/><br/>
-                    <div class="ui two column grid">
-                    <div class="column center aligned">
-                        <i class="html5 icon techLogo" style={{"color":"rgb(255, 141, 1)"}}></i>
+                    <div className="ui two column grid">
+                    <div className="column center aligned tech">
+                        <i className="html5 icon techLogo" style={{"color":"rgb(255, 141, 1)"}}></i>
                         <br/>
                         HTML5
                     </div>
-                    <div class="column center aligned">
-                        <i class="database icon techLogo" style={{"color":"rgb(29, 143, 122)"}}></i>
+                    <div className="column center aligned tech">
+                        <i className="database icon techLogo" style={{"color":"rgb(29, 143, 122)"}}></i>
                         <br/>
                         Different DBMS paradigms
                     </div>  
                     </div>
                 </div>
+                <br/><br/>
             </div>
             <div className="section secondary-color">
                 <div className="publications hidden">
@@ -129,13 +129,11 @@ class Home extends Component {
                         I frequently write about my work.<br/>
                         Here are some of my latest articles :<br/>
                     </p>
-                    <p>
-                        {this.state.feed.map((item, index) => {
-                            return (
-                                <h4 key={index} class="feed-item-title"><a href={item.link} target="blank">{item.title}</a></h4>
-                            )
-                        })}
-                    </p>
+                    {this.state.feed.map((item, index) => {
+                        return (
+                            <h4 key={index} className="feed-item-title"><a href={item.link} target="blank">{item.title}</a></h4>
+                        )
+                    })}
                 </div>
             </div>
             <div className="separator waves2"> </div>
